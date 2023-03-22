b8c5d364f6b9aa91a6c44a8ee667e4bf28933eb9
Dm Sd <maeestrus@gmail.com>
2023-03-22 09:23:02 +0500

    Added gunicorn server

--- init.sh
+++ init.sh
@@ -9,13 +9,13 @@ if [ ! -e "${path}etc/nginx.conf" ]
 then
     cat << "EOF" >> ${path}etc/nginx.conf
 server {
-    listen 80 default_server;
+    listen 80;
     error_log /var/log/nginx/test.log info;
-    proxy_set_header Host $host;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 
-    location ^~ /hello/ {
+    location / {
         proxy_pass https://0.0.0.0:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
 }
 EOF
@@ -24,22 +24,22 @@ else
     cat ${path}etc/nginx.conf
 fi
 
-if [ ! -e "${path}etc/hello.py" ]
+if [ ! -e "${path}hello.py" ]
 then
-    cat << "EOF" >> ${path}etc/hello.py
+    cat << "EOF" >> ${path}hello.py
 bind = "0.0.0.0:8080"
 
 def app(environ, start_response):
-    data = (bytes(i, encoding="utf-8") for i in environ['QUERY_STRING'].split("&"))
+    data = (bytes(i + "\n", encoding="utf-8") for i in environ['QUERY_STRING'].split("&"))
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return data
 EOF
 else
-    echo "The file ${path}etc/hello.py already exists"
-    cat ${path}etc/hello.py
+    echo "The file ${path}hello.py already exists"
+    cat ${path}hello.py
 fi
 sudo ln -sf ${path}etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd ${path}/web/etc && gunicorn -c python:hello hello:app &
+cd ${path}etc && gunicorn -c file:${path}hello.py hello:app &
